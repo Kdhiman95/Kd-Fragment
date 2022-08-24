f0587137652fef15b0cb643c528492e34e216095
Kamal Dhiman <kamal.dhiman2001@gmail.com>
2022-08-24 16:08:26 +0530

    Implemented Back Stack

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -1,22 +1,56 @@
 package com.example.fragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.fragment.fragments.FragmentA
+import com.example.fragment.fragments.FragmentB
+import com.example.fragment.fragments.FragmentC
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var frag : FragmentManager
+    private lateinit var ft : FragmentTransaction
+    private lateinit var btnFragA : Button
+    private lateinit var btnFragB : Button
+    private lateinit var btnFragC : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadFrg(FragmentA(),0)
+
+        btnFragA = findViewById(R.id.FragA)
+        btnFragA.setOnClickListener {
+            loadFrg(FragmentA(),0)
+        }
+
+        btnFragB = findViewById(R.id.FragB)
+        btnFragB.setOnClickListener{
+            loadFrg(FragmentB(),1)
+        }
+
+        btnFragC = findViewById(R.id.FragC)
+        btnFragC.setOnClickListener{
+            loadFrg(FragmentC(),1)
+        }
+
+    }
+    private fun loadFrg(fragment : Fragment, flag : Int) {
         frag = supportFragmentManager
-
-
-        val fragmentA = FragmentA()
-        frag.beginTransaction().add(R.id.fragment_container, fragmentA).commit()
+        ft = frag.beginTransaction()
+        if (flag == 0) {
+            ft.add(R.id.fragment_container, fragment)
+            frag.popBackStack("ROOT", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+            ft.addToBackStack("ROOT")
+        } else {
+            ft.replace(R.id.fragment_container, fragment)
+            ft.addToBackStack(null)
+        }
+        ft.commit()
     }
 }
--- app/src/main/java/com/example/fragment/fragments/FragmentC.kt
+++ app/src/main/java/com/example/fragment/fragments/FragmentC.kt
@@ -1,25 +1,19 @@
 package com.example.fragment.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fragment.R
 
-class FragmentA : Fragment() {
+class FragmentC : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("FG", "onResume: A")
+        return inflater.inflate(R.layout.fragment_c, container, false)
     }
 }
